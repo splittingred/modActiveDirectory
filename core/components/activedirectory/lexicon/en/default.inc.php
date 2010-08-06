3b61fb513ae572344e8418e183a86406cf1e1171
Shaun McCormick <shaun@modxcms.com>
2010-08-06 09:42:51 -0500

    Copyright info additions

--- core/components/activedirectory/lexicon/en/default.inc.php
+++ core/components/activedirectory/lexicon/en/default.inc.php
@@ -1,5 +1,29 @@
 <?php
 /**
+ * modActiveDirectory
+ *
+ * Copyright 2010 by Shaun McCormick <shaun@modx.com>
+ *
+ * This file is part of modActiveDirectory, which integrates Active Directory
+ * authentication into MODx Revolution.
+ *
+ * modActiveDirectory is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * modActiveDirectory is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * modActiveDirectory; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package activedirectory
+ */
+/**
  * Default English language file for ActiveDirectory
  *
  * @package activedirectory
